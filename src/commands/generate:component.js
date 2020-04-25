83dc62a9eefebaf8a31999b0d41fd218cc557015
Evandro Ribeiro <ribeiroevandro@live.com>
2020-04-25 13:01:14 -0300

    feat(): formated names on the create Screen or Components

--- src/commands/generate:component.js
+++ src/commands/generate:component.js
@@ -1,10 +1,11 @@
 module.exports = {
   name: 'generate:component',
+  alias: 'cc',
   description: 'Create new component inside src/components',
   run: async toolbox => {
-    const { parameters, createComponent } = toolbox
+    const { parameters, createComponent, strings } = toolbox
 
-    const name = parameters.first
+    const name = strings.pascalCase(parameters.string)
 
     await createComponent('src/components', name)
   }
--- src/commands/generate:screen.js
+++ src/commands/generate:screen.js
@@ -1,10 +1,11 @@
 module.exports = {
   name: 'generate:screen',
+  alias: 'cs',
   description: 'Create new page inside src/screens',
   run: async toolbox => {
-    const { parameters, createComponent } = toolbox
+    const { parameters, createComponent, strings } = toolbox
 
-    const name = parameters.first
+    const name = strings.pascalCase(parameters.string)
 
     await createComponent('src/screens', name)
   }
